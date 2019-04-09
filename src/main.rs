b6bc68732d1a047c79319e02dd4cd10f2523b047
ikubaku <hide4d51@gmail.com>
2019-04-09 21:23:52 +0900

    main.rsを追加。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::env;
+
+fn run<'a>() -> Result<(), &'a str> {
+    if env::args().len() != 2 {
+        Err("引数の個数が正しくありません")
+    } else {
+        let n_parsed: i64;
+        let res = env::args().nth(1).unwrap().parse::<i64>();
+        if res.is_err() {
+            Err("ソースコードをパースできません")
+        } else {
+            n_parsed = res.unwrap();
+
+            println!(".intel_syntax noprefix");
+            println!(".global main");
+            println!("main:");
+            println!("  mov rax, {}", n_parsed);
+            println!("  ret");
+
+            Ok(())
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    ::std::process::exit(match run() {
+        Ok(_) => 0,
+        Err(err) => {
+            eprintln!("Error: {:?}", err);
+            1
+        }
+    });
 }
